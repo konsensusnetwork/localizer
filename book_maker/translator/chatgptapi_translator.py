032735fb5ad08b5a5d1c6620cc18a8e91a79befb
Thibaut <thibaut@konsensus.network>
2025-03-05 16:51:20 +0100

    Remove deprecated model from list

--- book_maker/translator/chatgptapi_translator.py
+++ book_maker/translator/chatgptapi_translator.py
@@ -26,7 +26,6 @@ GPT35_MODEL_LIST = [
     "gpt-3.5-turbo-1106",
     "gpt-3.5-turbo-16k",
     "gpt-3.5-turbo-0613",
-    "gpt-3.5-turbo-16k-0613",
     "gpt-3.5-turbo-0301",
     "gpt-3.5-turbo-0125",
 ]
